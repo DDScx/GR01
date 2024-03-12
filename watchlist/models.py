de640385260182392df7a1f8f667f557bd3d9ed9
DDScx <162294452+DDScx@users.noreply.github.com>
2024-03-12 22:49:32 +0800

    0.1版本 已实现:①mysql数据库简单连接。②用户登录简单实现。 未实现：①注册。②个人中心。③主页模块。④人物卡模块。⑤车卡模块。⑥完善规则书模块。

--- watchlist/models.py
+++ watchlist/models.py
@@ -3,10 +3,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from watchlist import db
 
-class User(db.Model, UserMixin): # 用户表
+
+class User(db.Model, UserMixin):  # 用户表
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(255))
     nickname = db.Column(db.String(20))
 
     def set_password(self, password):
